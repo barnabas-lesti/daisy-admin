22061fe6f5b1e9b248c4d71108977ae0324d3c0a
Barnabas Lesti <barnabas.lesti@gmail.com>
2019-03-06 07:02:01 +0100

    Implemented API endpoint.

--- src/server/routes/exercises.js
+++ src/server/routes/exercises.js
@@ -1,12 +1,12 @@
 const restifyErrors = require('restify-errors');
 
-const Food = require('../models/Food');
+const Exercise = require('../models/Exercise');
 
 const convertParamsToDocument = params => params;
 const convertDocumentToResponse = doc => doc;
 
 module.exports = server => {
-	server.get('/api/food', async (req, res, next) => {
+	server.get('/api/exercises', async (req, res, next) => {
 		const { searchQuery } = req.query;
 		const findConditions = {};
 		if (searchQuery) {
@@ -18,33 +18,33 @@ module.exports = server => {
 				'i'
 			);
 		}
-		const docs = await Food.find(findConditions);
+		const docs = await Exercise.find(findConditions);
 		res.send(docs.map(doc => convertDocumentToResponse(doc)));
 		return next();
 	});
 
-	server.get('/api/food/:_id', async (req, res, next) => {
-		const doc = await Food.findById(req.params._id);
+	server.get('/api/exercises/:_id', async (req, res, next) => {
+		const doc = await Exercise.findById(req.params._id);
 		res.send(doc !== null ? convertDocumentToResponse(doc) : new restifyErrors.NotFoundError());
 		return next();
 	});
 
-	server.put('/api/food', async (req, res, next) => {
-		const doc = await Food.create(convertParamsToDocument(req.params));
+	server.put('/api/exercises', async (req, res, next) => {
+		const doc = await Exercise.create(convertParamsToDocument(req.params));
 		res.send(convertDocumentToResponse(doc));
 		return next();
 	});
 
-	server.patch('/api/food/:_id', async (req, res, next) => {
+	server.patch('/api/exercises/:_id', async (req, res, next) => {
 		const { _id } = req.params;
-		const result = await Food.updateOne({ _id }, convertParamsToDocument(req.params));
+		const result = await Exercise.updateOne({ _id }, convertParamsToDocument(req.params));
 		res.send(result.n > 0 ? true : new restifyErrors.NotFoundError());
 		return next();
 	});
 
-	server.del('/api/food/:_id', async (req, res, next) => {
+	server.del('/api/exercises/:_id', async (req, res, next) => {
 		const { _id } = req.params;
-		const result = await Food.deleteOne({ _id });
+		const result = await Exercise.deleteOne({ _id });
 		res.send(result.n > 0 ? true : new restifyErrors.NotFoundError());
 		return next();
 	});
